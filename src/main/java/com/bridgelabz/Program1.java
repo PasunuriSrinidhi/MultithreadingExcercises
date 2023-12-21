66cb7f1c622a48e777c5b93e870a7963b8025b12
Pasunuri Srinidhi <93040752+PasunuriSrinidhi@users.noreply.github.com>
2023-12-21 13:12:24 +0530

    to create and start multiple threads that increment a shared counter variable

--- src/main/java/com/bridgelabz/Program1.java
+++ src/main/java/com/bridgelabz/Program1.java
@@ -1,2 +1,59 @@
-package com.bridgelabz;public class Program1 {
+package com.bridgelabz;
+
+class Counter {
+    private int count = 0;
+
+    public synchronized void increment() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
 }
+
+class IncrementThread extends Thread {
+    private Counter counter;
+    private int increments;
+
+    public IncrementThread(Counter counter, int increments) {
+        this.counter = counter;
+        this.increments = increments;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < increments; i++) {
+            counter.increment();
+        }
+    }
+}
+
+public class Program1 {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        int numThreads = 5;
+        int incrementsPerThread = 1000;
+
+        Thread[] threads = new Thread[numThreads];
+
+        // Create and start threads
+        for (int i = 0; i < numThreads; i++) {
+            threads[i] = new IncrementThread(counter, incrementsPerThread);
+            threads[i].start();
+        }
+
+        // Wait for all threads to finish
+        try {
+            for (Thread thread : threads) {
+                thread.join();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Print the final count
+        System.out.println("Final count: " + counter.getCount());
+    }
+}
+
